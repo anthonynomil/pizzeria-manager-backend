758fea7ef488b547cb01dc7a2449df9a1ac9ed53
Anthony Limon <a.limon@inara.fr>
2023-09-23 18:50:57 +0200

    Added moderator role and setup auth for ingredients routes

--- src/routes/ingredient.routes.ts
+++ src/routes/ingredient.routes.ts
@@ -2,12 +2,14 @@ import { Router } from "express";
 import validate from "middlewares/validate";
 import ingredientValidator from "validator/ingredient.validator";
 import ingredientController from "controllers/ingredient.controller";
+import userRole from "const/enums/user.roles";
+import auth from "middlewares/auth";
 
 const router = Router();
 
-router.post("/", validate(ingredientValidator.create), ingredientController.create);
-router.get("/:ingredientId", validate(ingredientValidator.getById), ingredientController.getById);
-router.put("/:ingredientId", validate(ingredientValidator.update), ingredientController.update);
-router.delete("/:ingredientId", validate(ingredientValidator.remove), ingredientController.remove);
+router.post("/", auth(userRole.MODERATOR), validate(ingredientValidator.create), ingredientController.create);
+router.get("/:ingredientId", auth(userRole.MODERATOR), validate(ingredientValidator.getById), ingredientController.getById);
+router.put("/:ingredientId", auth(userRole.MODERATOR), validate(ingredientValidator.update), ingredientController.update);
+router.delete("/:ingredientId", auth(userRole.MODERATOR), validate(ingredientValidator.remove), ingredientController.remove);
 
 export default router;
