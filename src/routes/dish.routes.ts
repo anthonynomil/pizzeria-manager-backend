2ab75d9e645abafd5640620473e4efb092d09a9e
Anthony Limon <a.limon@inara.fr>
2023-10-14 12:18:26 +0200

    Added auth to dish routes

--- src/routes/dish.routes.ts
+++ src/routes/dish.routes.ts
@@ -2,13 +2,15 @@ import { Router } from "express";
 import validate from "middlewares/validate";
 import dishValidator from "validator/dish.validator";
 import dishController from "controllers/dish.controller";
+import auth from "middlewares/auth";
+import userRole from "const/enums/user.roles";
 
 const router = Router();
 
-router.post("/", validate(dishValidator.create), dishController.create);
-router.get("/:dishId", validate(dishValidator.getById), dishController.getById);
-router.put("/:dishId", validate(dishValidator.update), dishController.update);
-router.delete("/:dishId", validate(dishValidator.remove), dishController.remove);
+router.post("/", auth(userRole.MODERATOR), validate(dishValidator.create), dishController.create);
+router.get("/:dishId", auth(userRole.MODERATOR), validate(dishValidator.getById), dishController.getById);
+router.put("/:dishId", auth(userRole.MODERATOR), validate(dishValidator.update), dishController.update);
+router.delete("/:dishId", auth(userRole.MODERATOR), validate(dishValidator.remove), dishController.remove);
 
 router.post("/:dishId/ingredient/:ingredientId", validate(dishValidator.addIngredient), dishController.addIngredient);
 router.delete("/:dishId/ingredient/:ingredientId");
